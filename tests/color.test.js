7b35c7c7b6a7809a749fa1d0e7b0c8349af5840b
beaubouchard <beau@beaubouchard.com>
2019-01-07 00:08:30 -0500

    test: more tests for the color functions

--- tests/color.test.js
+++ tests/color.test.js
@@ -1,5 +1,33 @@
 const { color } = require('../');
 
-test('testing most basic yellow call', () => {
+test('should return test message with the ANSI color black before it and reset after', () => {
+  expect(color.black('test')).toBe(`\x1b[30mtest\x1b[0m`);
+});
+
+test('should return test message with the ANSI color red before it and reset after', () => {
+  expect(color.red('test')).toBe(`\x1b[31mtest\x1b[0m`);
+});
+
+test('should return test message with the ANSI color green before it and reset after', () => {
+  expect(color.green('test')).toBe(`\x1b[32mtest\x1b[0m`);
+});
+
+test('should return test message with the ANSI color yellow before it and reset after', () => {
   expect(color.yellow('test')).toBe(`\x1b[33mtest\x1b[0m`);
+});
+
+test('should return test message with the ANSI color blue before it and reset after', () => {
+  expect(color.blue('test')).toBe(`\x1b[34mtest\x1b[0m`);
+});
+
+test('should return test message with the ANSI color magenta before it and reset after', () => {
+  expect(color.magenta('test')).toBe(`\x1b[35mtest\x1b[0m`);
+});
+
+test('should return test message with the ANSI color cyan before it and reset after', () => {
+  expect(color.cyan('test')).toBe(`\x1b[36mtest\x1b[0m`);
+});
+
+test('should return test message with the ANSI color white before it and reset after', () => {
+  expect(color.white('test')).toBe(`\x1b[37mtest\x1b[0m`);
 });
