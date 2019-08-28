e962c5215af66d8209d6a6a09dfa15b1057adfc2
beaubouchard <beau@beaubouchard.com>
2019-08-28 14:00:43 -0400

    chore: fixed the tests calls

--- tests/lib/utils/sanitize.test.js
+++ tests/lib/utils/sanitize.test.js
@@ -2,7 +2,7 @@ const chai = require('chai');
 const sinon = require('sinon');
 const sinonChai = require('sinon-chai');
 
-const Sanitize = require('../../../lib/utils/sanitize');
+const sanitize = require('../../../lib/utils/sanitize');
 
 chai.should();
 
@@ -14,42 +14,42 @@ describe('Sanitize Utility Tests', () => {
   describe('sanitize method', () => {
     it('should return empty string if nothing is passed in', () => {
       const expected = '';
-      const resp = Sanitize.sanitize();
+      const resp = sanitize();
       expect(resp).to.equal(expected);
     });
 
     it('should fix arrays of 1 length to be string', () => {
       const testData = ['tacos'];
       const expected = 'tacos';
-      const resp = Sanitize.sanitize(testData);
+      const resp = sanitize(testData);
       expect(resp).to.equal(expected);
     });
 
     it('should return empty string if empty object is passed in', () => {
       const testData = {};
       const expected = '';
-      const resp = Sanitize.sanitize(testData);
+      const resp = sanitize(testData);
       expect(resp).to.equal(expected);
     });
 
     it('should remove br tags', () => {
       const testData = '</br>';
       const expected = '';
-      const resp = Sanitize.sanitize(testData);
+      const resp = sanitize(testData);
       expect(resp).to.equal(expected);
     });
 
     it('should remove br tag and add rn line', () => {
       const testData = 'tacos</br>tacos';
       const expected = 'tacos\ntacos';
-      const resp = Sanitize.sanitize(testData);
+      const resp = sanitize(testData);
       expect(resp).to.equal(expected);
     });
 
     it('should remove empty lines', () => {
       const testData = 'tacos</br></br></br>tacos';
       const expected = 'tacos\ntacos';
-      const resp = Sanitize.sanitize(testData);
+      const resp = sanitize(testData);
       expect(resp).to.equal(expected);
     });
   });
